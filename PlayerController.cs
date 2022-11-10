6ffa5648feb2ee1c74a0894eb523b3c3f92808f4
whitehead <sota.takase@gmail.com>
2022-11-10 20:18:51 +0900

    [Add] scripts

---
+++ PlayerController.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 3; //移動速度
+    [SerializeField] private float jumpPower = 3; //ジャンプ力
+    [SerializeField] private Animator animator;
+    private CharacterController _characterController; //キャッシュ
+    private Transform _transform; //キャッシュ
+    private Vector3 _moveVelocity; //移動速度情報
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _characterController = GetComponent<CharacterController>(); //マイフレームアクセスするので負荷を下げるためにキャッシュしておく
+        _transform = transform; //同じくキャッシュ
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(_characterController.isGrounded ? "地上にいます" : "空中です");
+
+        //入力軸による移動処理
+        _moveVelocity.x = Input.GetAxis("Horizontal") * moveSpeed;
+        _moveVelocity.z = Input.GetAxis("Vertical") * moveSpeed;
+
+        //移動方向に向く
+        _transform.LookAt(_transform.position + new Vector3(_moveVelocity.x, 0, _moveVelocity.z));
+
+        if (_characterController.isGrounded)
+        {
+            if (Input.GetButtonDown("Jump"))
+            {
+                //ジャンプ処理
+                Debug.Log("ジャンプ！");
+                _moveVelocity.y = jumpPower;
+            }
+        }
+        else
+        {
+            //重力による加速
+            _moveVelocity.y += Physics.gravity.y * Time.deltaTime;
+        }
+
+        //オブジェクトを動かす
+        _characterController.Move(_moveVelocity * Time.deltaTime);
+
+        //移動スピードをanimatorに反映
+        animator.SetFloat("MoveSpeed", new Vector3(_moveVelocity.x, 0, _moveVelocity.z).magnitude);
+    }
+}
