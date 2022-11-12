c33401e82c2e8d7f2a7db353b8aecf1ed901a38f
whitehead <sota.takase@gmail.com>
2022-11-12 19:34:40 +0900

    [Add] CollisionDetector

---
+++ CollisionDetector.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using System;
+
+[RequireComponent(typeof(Collider))]
+public class CollisionDetector : MonoBehaviour
+{
+    
+    [SerializeField] private TriggerEvent onTriggerStay = new TriggerEvent();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //is TriggerがONで他のColliderと重なっているときは、このメソッドが常にコールされる
+    private void OnTriggerStay(Collider other)
+    {
+        //onTriggerStayで指定された処理を実行する
+        onTriggerStay.Invoke(other);
+    }
+
+    //UnityEventを継承したクラスに[Serializable]属性を付与することで、Inspectorウィンドウ上に表示できるようになる
+    [Serializable] 
+    public class TriggerEvent : UnityEvent<Collider>
+    {
+      
+    }
+}
